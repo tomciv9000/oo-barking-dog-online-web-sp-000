1235392f10c6b88c4074eabf09a1ef9f00630007
Tom White <eugene.white@gmail.com>
2019-05-29 19:16:38 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Dog
+
+  def name=(doggie_name)
+    @dogs_name = doggie_name
+  end
+
+  def name
+    @dogs_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+
+end
